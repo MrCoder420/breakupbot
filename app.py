f306afc038d3a7555b34dcf635b2a0ea2f6193db
Mrcoder420 <nikhilbhor201@gmail.com>
2026-05-02 10:25:53 +0530

    Fix Python 3.9 compatibility (Optional type hint)

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from datetime import datetime, timedelta
+from typing import Optional
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
@@ -103,7 +104,7 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
