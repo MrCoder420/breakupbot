0fdf1152216649bcb16a54b01c05611df1bb9f5e
Mrcoder420 <nikhilbhor201@gmail.com>
2026-05-02 10:56:47 +0530

    Feat: Added Vercel proxy rewrite to bypass Mixed Content error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://52.66.203.85:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
